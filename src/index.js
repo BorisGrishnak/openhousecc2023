e56b8a757df49c7b62ec2dc3e14b2808714be8c6
Boris Grishnak <58435887+BorisGrishnak@users.noreply.github.com>
2023-10-11 14:17:07 +0700

    router switch hashr

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
